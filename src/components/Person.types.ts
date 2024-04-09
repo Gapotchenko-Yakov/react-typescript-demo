df29b029f2a1254806da244778c0555cb80be3e6
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-09 14:44:11 +0300

    Condiional type demo.

--- src/components/Person.types.ts
+++ src/components/Person.types.ts
@@ -14,3 +14,16 @@ export type PersonProps = Person;
 
 type I1 = Person["name" | "occupation"];
 type I2 = Person[keyof Person];
+
+interface IPerson extends Person {}
+
+interface IEmployee extends IPerson {
+  access: 2;
+}
+
+interface INonEmployee extends IPerson {
+  access: false;
+}
+
+type Example1 = IEmployee extends IPerson ? number : string;
+type Example2 = INonEmployee extends IPerson ? number : string;
