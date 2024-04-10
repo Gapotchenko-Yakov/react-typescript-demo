e07559e00f9ebccd39c2f5d4047a42d0cc590bf5
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-10 11:25:35 +0300

    Event props demo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
-
 import "./App.css";
-import Person from "./components/Person";
-import Counter from "./components/Counter";
+
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
+  const [value, setValue] = useState("some text");
+
   return (
     <div>
-      <Counter />
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked", event, id);
+        }}
+      />
+      <Input
+        value={value}
+        handleChange={(event) => {
+          setValue(event.target.value);
+        }}
+      />
     </div>
   );
 }
