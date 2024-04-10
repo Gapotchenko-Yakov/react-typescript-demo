e3590588b245e90fe95c4b8445dccf46e46fe7e7
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-10 11:36:16 +0300

    Style props demo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 
-import Button from "./components/Button";
-import Input from "./components/Input";
+import Container from "./components/Container";
 
 function App() {
   const [value, setValue] = useState("some text");
 
   return (
     <div>
-      <Button
-        handleClick={(event, id) => {
-          console.log("Button clicked", event, id);
-        }}
-      />
-      <Input
-        value={value}
-        handleChange={(event) => {
-          setValue(event.target.value);
-        }}
-      />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
     </div>
   );
 }
